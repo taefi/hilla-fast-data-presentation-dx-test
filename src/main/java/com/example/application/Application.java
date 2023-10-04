601afcaab36cefe6711a9060058e75549059875c
taefi <taefi.soroosh@gmail.com>
2023-10-04 20:14:35 +0300

    fix theme name

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "hilla-autogrid-dx-test")
+@Theme(value = "hilla-dx-test")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
